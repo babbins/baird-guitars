65526db2f251065a5a37b96837c3f6036c9750d3
babbins <thealimahmoud@gmail.com>
2020-08-04 23:07:01 -0400

    add gallery instructions and style horizontal gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,6 +2,7 @@ import React from "react";
 import Img from "gatsby-image";
 import mq from "../utils/mediaQueries";
 import { Link } from "gatsby";
+import { css } from "@emotion/core";
 
 const GalleryImage = ({ image, className }) => (
   <Img className={className} fluid={image.childImageSharp.fluid} />
@@ -15,38 +16,48 @@ const GalleryItemCss = {
 };
 
 const Gallery = ({ galleryItems }) => (
-  <div
-    css={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexWrap: "wrap",
-      margin: "0 auto",
-      width: "90%",
-    }}
-  >
-    {galleryItems.map(({ image, url }) => {
-      if (url) {
-        return (
-          <Link
-            css={GalleryItemCss}
-            key={image.childImageSharp.fluid.src}
-            to={url}
-          >
-            <GalleryImage image={image} />
-          </Link>
-        );
-      } else {
-        return (
-          <GalleryImage
-            css={GalleryItemCss}
-            key={image.childImageSharp.fluid.src}
-            image={image}
-          />
-        );
-      }
-    })}
-  </div>
+  <>
+    <h2
+      css={css`
+        text-align: center;
+        margin-top: 20px;
+      `}
+    >
+      Click an image for details
+    </h2>
+    <div
+      css={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+        margin: "0 auto",
+        width: "90%",
+      }}
+    >
+      {galleryItems.map(({ image, url }) => {
+        if (url) {
+          return (
+            <Link
+              css={GalleryItemCss}
+              key={image.childImageSharp.fluid.src}
+              to={url}
+            >
+              <GalleryImage image={image} />
+            </Link>
+          );
+        } else {
+          return (
+            <GalleryImage
+              css={GalleryItemCss}
+              key={image.childImageSharp.fluid.src}
+              image={image}
+            />
+          );
+        }
+      })}
+    </div>
+  </>
 );
 
 export default Gallery;
--- src/components/HorizontalGallery.js
+++ src/components/HorizontalGallery.js
@@ -3,6 +3,7 @@ import Img from "gatsby-image";
 import mq from "../utils/mediaQueries";
 import { Link } from "gatsby";
 import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
 const GalleryImage = ({ image, className }) => (
   <Img className={className} fluid={image.childImageSharp.fluid} />
@@ -25,45 +26,59 @@ const GalleryItemCss = {
 };
 
 const HorizontalGallery = ({ galleryItems }) => (
-  <div>
-    <h3 css={{ marginBottom: "10px" }}>
-      For pricing or more information on any available instruments please
-      contact me
-    </h3>
-    <ContactLink href="tel:718-344-8587">Phone: 718-344-8587</ContactLink>
-    <ContactLink href="mailto:bairdguitars@gmail.com">
-      Email: bairdguitars@gmail.com
-    </ContactLink>
-    <div
-      css={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-        flexWrap: "wrap",
-      }}
+  <>
+    <h2
+      css={css`
+        text-align: center;
+        margin: 20px;
+      `}
     >
-      {galleryItems.map(({ image, url, name, description }) => {
-        return (
-          <div css={GalleryItemCss} key={image.childImageSharp.fluid.src}>
-            <Link to={url} css={{ color: "#200 !important" }}>
-              <h3>{name}</h3>
-              <GalleryImage
-                css={{
-                  width: "80%",
-                  [mq[0]]: {
-                    width: "500px",
-                  },
-                }}
-                image={image}
-              />
-            </Link>
-            <span>{description}</span>
-          </div>
-        );
-      })}
+      Click an image for details
+    </h2>
+    <div
+      css={css`
+        margin: 20px;
+      `}
+    >
+      <h3 css={{ marginBottom: "10px" }}>
+        For pricing or more information on any available instruments please
+        contact me
+      </h3>
+      <ContactLink href="tel:718-344-8587">Phone: 718-344-8587</ContactLink>
+      <ContactLink href="mailto:bairdguitars@gmail.com">
+        Email: bairdguitars@gmail.com
+      </ContactLink>
+      <div
+        css={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "flex-start",
+          flexWrap: "wrap",
+        }}
+      >
+        {galleryItems.map(({ image, url, name, description }) => {
+          return (
+            <div css={GalleryItemCss} key={image.childImageSharp.fluid.src}>
+              <Link to={url} css={{ color: "#200 !important" }}>
+                <h3>{name}</h3>
+                <GalleryImage
+                  css={{
+                    width: "80%",
+                    [mq[0]]: {
+                      width: "500px",
+                    },
+                  }}
+                  image={image}
+                />
+              </Link>
+              <span>{description}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
+  </>
 );
 
 export default HorizontalGallery;
